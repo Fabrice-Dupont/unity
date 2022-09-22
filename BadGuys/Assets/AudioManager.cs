0a8df6e4a652df0041e1a8946d79bf549c1532db
laetitia fortineau <laetitia.fortineau@gmail.com>
2022-09-22 23:42:24 +0200

    Musique 8bit qui claque sa maman!

---
+++ BadGuys/Assets/AudioManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] playlist;
+    public AudioSource audiosource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource.clip = playlist[0];
+        audiosource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
